7acf8cbb7293bad2a2c811e975dc27ba9535c721
Sidd1542004 <siddarth15042004@gmail.com>
2025-06-29 19:41:49 +0530

    Update BankAccountTest.java

--- Week2/JUnit Testing Exercises/Exercise_4_AAA_Pattern/BankAccountTest.java
+++ Week2/JUnit Testing Exercises/Exercise_4_AAA_Pattern/BankAccountTest.java
@@ -3,36 +3,30 @@ import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class BankAccountTest {
-
+public class BankAccountTest{
     private BankAccount account;
-
     @Before
-    public void setUp() {
-        account = new BankAccount("Alice", 1000);
+    public void setUp(){
+        account=new BankAccount("Alice",1000);
     }
-
     @After
-    public void tearDown() {
-        account = null;
+    public void tearDown(){
+        account=null;
     }
-
     @Test
-    public void testDeposit() {
+    public void testDeposit(){
         account.deposit(500);
-        assertEquals(1500, account.getBalance());
+        assertEquals(1500,account.getBalance());
     }
-
     @Test
-    public void testWithdraw() {
+    public void testWithdraw(){
         account.withdraw(300);
         assertEquals(700, account.getBalance());
     }
-
     @Test
-    public void testWithdrawMoreThanBalance() {
+    public void testWithdrawMoreThanBalance(){
         account.withdraw(2000);
-        assertEquals(1000, account.getBalance());
+        assertEquals(1000,account.getBalance());
     }
 
     @Test
