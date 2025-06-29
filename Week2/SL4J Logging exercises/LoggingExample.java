0a216cf6312f27e7b39cd5b3135917b8775cd819
Sidd1542004 <siddarth15042004@gmail.com>
2025-06-29 20:14:41 +0530

    Update LoggingExample.java

--- Week2/SL4J Logging exercises/LoggingExample.java
+++ Week2/SL4J Logging exercises/LoggingExample.java
@@ -1,11 +1,11 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-public class LoggingExample {
-    private static final Logger logger = LoggerFactory.getLogger(LoggingExample.class);
-
-    public static void main(String[] args) {
-        logger.error("This is an error message");
-        logger.warn("This is a warning message");
+public class PaymentProcessor{
+    private static final Logger logger=LoggerFactory.getLogger(PaymentProcessor.class);
+    public static void main(String[] args){
+        logger.info("Payment processing started...");
+        logger.debug("Connecting to payment gateway...");
+        logger.warn("Transaction taking longer than expected.");
+        logger.error("Payment failed due to insufficient balance.");
     }
 }
