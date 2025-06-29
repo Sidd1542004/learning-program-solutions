69a64cd50f25abdc76fc2127ff58f4e4deafbe7f
Sidd1542004 <siddarth15042004@gmail.com>
2025-06-29 19:25:05 +0530

    Update CalculatorTest.java

--- Week2/JUnit Testing Exercises/Exercise_1_Setting_Up_JUnit/CalculatorTest.java
+++ Week2/JUnit Testing Exercises/Exercise_1_Setting_Up_JUnit/CalculatorTest.java
@@ -1,14 +1,26 @@
-
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class CalculatorTest {
-
+public class CalculatorTest{
+    Calculator calc=new Calculator();
     @Test
-    public void testAdd() {
-        Calculator calc = new Calculator();
-        int result = calc.add(8, 7);
-        assertEquals(15, result);
+    public void testAdd(){
+        assertEquals(5,calc.add(2,3));
+    }
+    @Test
+    public void testSubtract(){
+        assertEquals(1,calc.subtract(3,2));
+    }
+    @Test
+    public void testMultiply(){
+        assertEquals(6,calc.multiply(2,3));
+    }
+    @Test
+    public void testDivide(){
+        assertEquals(2.0,calc.divide(4,2),0.001);
+    }
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero(){
+        calc.divide(5, 0);
     }
 }
